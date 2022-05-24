d3e06ad05a586f2b5da7a34ddbe6fa671ac018d8
challenge-khs <hanseong0826@gmail.com>
2022-05-24 22:05:35 +0900

    Tree

---
+++ src/tree/treeTraversal.java
@@ -0,0 +1,79 @@
+package tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class treeTraversal {
+    static class Node {
+        char value;
+        Node left;
+        Node right;
+
+        public Node(char value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static void insertNode(Node temp, char root, char left, char right) {
+        if(temp.value == root) {
+            temp.left = (left == '.' ? null : new Node(left, null, null));
+            temp.right = (right == '.' ? null : new Node(right, null, null));
+        } else {
+            if(temp.left != null)
+                insertNode(temp.left, root, left, right);
+            if(temp.right != null)
+                insertNode(temp.right, root, left, right);
+        }
+    }
+
+    static void preorder(Node node) {
+        if(node == null)
+            return;
+        System.out.println(node.value);
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    static void inorder(Node node) {
+        if(node == null)
+            return;
+        inorder(node.left);
+        System.out.println(node.value);
+        inorder(node.right);
+    }
+
+    static void postorder(Node node) {
+        if(node == null)
+            return;
+        postorder(node.left);
+        postorder(node.right);
+        System.out.println(node.value);
+    }
+
+    static Node head = new Node('A', null, null);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char root = st.nextToken().charAt(0);
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
+
+            insertNode(head, root, left, right);
+        }
+
+        preorder(head);
+        System.out.println();
+        inorder(head);
+        System.out.println();
+        postorder(head);
+        System.out.println();
+    }
+}
